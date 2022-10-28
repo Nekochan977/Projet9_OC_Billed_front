19f89452a8adaf97004c90d1dfafa872ca885819
Nekochan977 <nadja.talaouitmockli@yahoo.com>
2022-10-28 14:41:13 +0200

    NewBill POST test added

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -11,9 +11,12 @@ import {localStorageMock} from "../__mocks__/localStorage.js"
 import router from "../app/Router.js"
 // import mockedStore très important apparemment - penser à demander à Antoine...
 import mockedStore from "../__mocks__/store"
+import { async } from "regenerator-runtime";
+import BillsUI from "../views/BillsUI.js";
 
 
 describe("Given I am connected as an employee", () => {
+
   describe("When I am on NewBill Page", () => {
     test("Then NewBill page should be rendered", () => {
       const html = NewBillUI()
@@ -23,6 +26,7 @@ describe("Given I am connected as an employee", () => {
       expect(screen.getByTestId('form-new-bill')).toBeTruthy()
     })
   })
+
   describe("When I do not fill fields and I click on submit button", () => {
     test("Then I should stay on NewBill page", () => {
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
@@ -40,7 +44,6 @@ describe("Given I am connected as an employee", () => {
 
       const newBill = new NewBill({ document, onNavigate, store: mockedStore, localStorage: window.localStorage })
       
-      //to-do write assertion
       const expenseName = screen.getByTestId("expense-name");
       expect(expenseName.value).toBe("");
 
@@ -68,6 +71,7 @@ describe("Given I am connected as an employee", () => {
 
     })
   })
+
   describe("When I upload the wong file format", () => {
     test("Alert message shoul appear", () => {
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
@@ -97,6 +101,74 @@ describe("Given I am connected as an employee", () => {
       expect(spyHandleChangeFile).toHaveBeenCalled()
       expect(window.alert).toHaveBeenCalledWith('Seuls des fichiers jpg, jpeg ou png sont acceptés.')
 
+    })
+  })
+
+  describe("POST NewBill Test", () => {
+    test("should create newbill and navigate to bills page", async () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }));
+    
+      document.body.innerHTML = NewBillUI();
+
+      const fakeBillData = {
+        type: 'Transports',
+        name: 'train Paris/Lyon',
+        date: '20-10-2021',
+        amount: '48',
+        vat: '4',
+        pct: '10',
+        commentary: 'Billet 2nd classe',
+        filename: 'billet-train',
+        fileUrl: 'C:\\fakepath\\billet-train.png'
+      };
+
+      const form = screen.getByTestId('form-new-bill');
+
+
+      fireEvent.change(screen.getByTestId('expense-type'), { target: { value: fakeBillData.type } });
+      fireEvent.change(screen.getByTestId('expense-name'), { target: { value: fakeBillData.name } });
+      fireEvent.change(screen.getByTestId('datepicker'), { target: { value: fakeBillData.date } });
+      fireEvent.change(screen.getByTestId('amount'), { target: { value: fakeBillData.amount } });
+      fireEvent.change(screen.getByTestId('vat'), { target: { value: fakeBillData.vat } });
+      fireEvent.change(screen.getByTestId('pct'), { target: { value: fakeBillData.pct } });
+      fireEvent.change(screen.getByTestId('commentary'), { target: { value: fakeBillData.commentary } });
+
+      // localStorage should be populated with form data
+      Object.defineProperty(window, "localStorage", {
+        value: {
+          getItem: jest.fn(() => null),
+          setItem: jest.fn(() => null),
+        },
+        writable: true,
+      });
+
+      // we have to mock navigation to test it
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+
+      let PREVIOUS_LOCATION = "NewBill";
+
+      const newBill = new NewBill({
+        document,
+        localStorage: window.localStorage,
+        onNavigate,
+        PREVIOUS_LOCATION,
+        store: mockedStore,
+      });
+
+      const handleSubmit = jest.fn(newBill.handleSubmit);
+      newBill.newBill = jest.fn().mockResolvedValue({});
+
+      form.addEventListener("submit", handleSubmit);
+      fireEvent.submit(form);
+      expect(handleSubmit).toHaveBeenCalled();
+      expect(handleSubmit).toHaveBeenCalled();
+      expect(screen.getAllByText("Mes notes de frais")).toBeTruthy();
+        
     })
   })
 
